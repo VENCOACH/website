f51470e16efb1e6ae62dccd945b2c6c118bff661
Carlos Giner <carlosginer90@gmail.com>
2022-04-06 15:52:34 +0200

    Feature | Add woman images and media queries to landing page

--- content/mainContent/index.jsx
+++ content/mainContent/index.jsx
@@ -2,18 +2,21 @@ import styles from './mainContent.module.scss';
 
 export default function MainContent() {
     
-    const { grid } = styles;
+    const { container, btnPrimary, deco_moon, woman_white, woman_gray } = styles;
 
     return (
-      <div className={grid}>
+      <div className={container}>
         <h1>
           SOMOS PROFESIONALES EN <span>COACHING</span> PERSONAL Y EMPRESARIAL
         </h1>
-        <img src="img/people-hero.png" alt="people" />
+        <img className={deco_moon} src="img/media-luna.png" />
+        <img className={woman_white} src="img/people-hero.png" alt="people" />
+        <img className={woman_gray} src="img/people-hero2.png" alt="people" />
         <p>
           Mas de 20 años trabajando de la mano de grandes profesionales, al
           servicio de una mejor gestión.
         </p>
+        <button className={btnPrimary}>Empezar!</button>
       </div>
     );
 }
