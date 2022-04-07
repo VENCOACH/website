bc52415497a3cc30eecc453aa7ae8c83ddd51ec3
Carlos Giner <carlosginer90@gmail.com>
2022-04-07 15:58:19 +0200

    Fix | Add space to span title

--- content/mainContent/index.jsx
+++ content/mainContent/index.jsx
@@ -15,7 +15,7 @@ export default function MainContent({sectionContentData}) {
     return (
       <div className={container}>
         <h1>
-          {titleFirstSlice}<span>{spanInTitle}</span> {titleSecondSlice}
+          {titleFirstSlice}<span>{` ${spanInTitle} `}</span> {titleSecondSlice}
         </h1>
         <img className={deco_moon} src="img/media-luna.png" />
         <img className={woman_gray} src="img/people-hero2.png" alt="people" />
