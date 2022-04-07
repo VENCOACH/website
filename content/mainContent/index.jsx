172804f34a73e4abbb1e10ea85c5be0ba8092deb
Carlos Giner <carlosginer90@gmail.com>
2022-04-07 15:41:00 +0200

    Feature | Complete main section layout and contentful data display

--- content/mainContent/index.jsx
+++ content/mainContent/index.jsx
@@ -1,22 +1,28 @@
 import styles from './mainContent.module.scss';
 
-export default function MainContent() {
-    
+export default function MainContent({sectionContentData}) {
+
+    const {
+      titleFirstSlice = {},
+      spanInTitle = {},
+      titleSecondSlice = {},
+      paragraph = {},
+      ctaButttonText = {}
+    } = sectionContentData.fields;
+  
     const { container, btnPrimary, deco_moon, woman_white, woman_gray } = styles;
 
     return (
       <div className={container}>
         <h1>
-          SOMOS PROFESIONALES EN <span>COACHING</span> PERSONAL Y EMPRESARIAL
+          {titleFirstSlice}<span>{spanInTitle}</span> {titleSecondSlice}
         </h1>
         <img className={deco_moon} src="img/media-luna.png" />
-        <img className={woman_white} src="img/people-hero.png" alt="people" />
         <img className={woman_gray} src="img/people-hero2.png" alt="people" />
         <p>
-          Mas de 20 años trabajando de la mano de grandes profesionales, al
-          servicio de una mejor gestión.
+          {paragraph}
         </p>
-        <button className={btnPrimary}>Empezar!</button>
+        <button className={btnPrimary}>{ctaButttonText}</button>
       </div>
     );
 }
