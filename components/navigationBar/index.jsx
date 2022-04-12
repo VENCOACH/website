bea93a73ddef36979dc4dc8fbbd7a152bee53157
Carlos Giner <carlosginer90@gmail.com>
2022-04-12 16:55:11 +0200

    Styling | Navigation Bar | Finish for desktop

--- components/navigationBar/index.jsx
+++ components/navigationBar/index.jsx
@@ -1,40 +1,58 @@
+import styles from './navigationBar.module.scss';
+
 export default function NavigationBar() {
+
+  const {wrapper, contentBox, upperSection, bottomSection, address, phone, rightSide, socialIcons, email} = styles;
+
   return (
-    <nav>
-      <section>
-        <div>
-          <img src="img/icon-location.png" alt="" />
-          <address>Calle 72 , av 10 .Edif Las americas,</address>
-          <div>
+    <nav className={wrapper}>
+      <section className={upperSection}>
+        <div className={contentBox}>
+          <div className={address}>
+            <img src="img/icon-location.png" alt="" />
+            <address>Calle 72 , av 10 .Edif Las americas,</address>
+          </div>
+          <div className={rightSide}>
+            <div className={phone}>
               <img src="img/icon-phone.png" alt="" />
               <p>+54 0424 6138945</p>
-          </div>
-          <div>
+            </div>
+            <div className={email}>
               <img src="img/icon-email.png" alt="" />
               <p>info@vencoach.org</p>
-          </div>
-          <div>
+            </div>
+            <div className={socialIcons}>
               <img src="img/icon-facebook.png" alt="" />
               <img src="img/icon-twitter.png" alt="" />
               <img src="img/icon-instagram.png" alt="" />
+            </div>
           </div>
         </div>
-        <div>
-            <phone></phone>
-        </div>
       </section>
-      <section>
-        <figure>
-          <img src="img/logo.png" alt="Vencoach Logo" />
-        </figure>
-        <ul>
-          <li>Nosotros</li>
-          <li>Servicios</li>
-          <li>Logros</li>
-          <li>Equipo</li>
-          <li>Testimonios</li>
-        </ul>
-        <button>Contactar</button>
+      <section className={bottomSection}>
+        <div className={contentBox}>
+          <figure>
+            <img src="img/logo.png" alt="Vencoach Logo" />
+          </figure>
+          <ul>
+            <li>
+              <a>Nosotros</a>
+            </li>
+            <li>
+              <a>Servicios</a>
+            </li>
+            <li>
+              <a>Logros</a>
+            </li>
+            <li>
+              <a>Equipo</a>
+            </li>
+            <li>
+              <a>Testimonios</a>
+            </li>
+          </ul>
+          <button>Contactar</button>
+        </div>
       </section>
     </nav>
   );
