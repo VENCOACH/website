017bd79469304494e5730a23f0a699460cce9742
Carlos Giner <carlosginer90@gmail.com>
2022-04-20 10:08:14 +0200

    Assets | Main | Add decorative halfmoon front

--- components/mainContent/index.jsx
+++ components/mainContent/index.jsx
@@ -10,15 +10,16 @@ export default function MainContent({sectionContentData}) {
       ctaButttonText = {}
     } = sectionContentData.fields;
   
-    const { container, btnPrimary, deco_moon, woman_white, woman_gray } = styles;
+    const { container, btnPrimary, deco_moon, deco_moon__front, woman_gray } = styles;
 
     return (
       <div className={container}>
         <h1>
           {titleFirstSlice}<span>{` ${spanInTitle} `}</span> {titleSecondSlice}
         </h1>
-        <img className={deco_moon} src="img/media-luna.png" />
+        <img className={deco_moon} src="img/media-luna.png" alt=""/>
         <img className={woman_gray} src="img/people-hero2.png" alt="people" />
+        <img className={deco_moon__front} src="img/decorative-halfmoon-front.png" alt="" />
         <p>
           {paragraph}
         </p>
