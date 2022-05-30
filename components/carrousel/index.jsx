8cc6cad39a5279f9eac9dbafc8157b2e44bb89d4
Carlos Giner <carlosginer90@gmail.com>
2022-05-30 21:05:51 +0200

    Feature | Add controls for slider testimonial section

--- components/carrousel/index.jsx
+++ components/carrousel/index.jsx
@@ -6,6 +6,26 @@ import Slider from "react-slick";
 
 export default class Carrousel extends Component {
 
+  constructor(props) {
+    super(props);
+    this.next = this.next.bind(this);
+    this.previous = this.previous.bind(this);
+  }
+
+  componentDidUpdate() {
+    if(this.props.next) {this.next()};
+    this.props.setNext(false);
+    if(this.props.back) {this.previous()};
+    this.props.setBack(false);
+  }
+
+  next() {
+    this.slider.slickNext();
+  }
+  previous() {
+    this.slider.slickPrev();
+  }
+
   render() {
     const settings = {
         dots: this.props.dots,
@@ -20,7 +40,7 @@ export default class Carrousel extends Component {
 
     return (
       <>
-        <Slider {...settings}>
+        <Slider {...settings} ref={c => (this.slider = c)}>
           {this.props.images.map((each, index) => (
             <div key={index} className={this.props.imageContainer}>
               <Image src={`https:${each.fields.file.url}`} layout="fill"  />
--- components/testimonials/index.jsx
+++ components/testimonials/index.jsx
@@ -1,6 +1,7 @@
 import Carrousel from "../carrousel";
 import InfoBlock from "../infoBlock";
 import styles from './testimonials.module.scss';
+import { useState } from "react";
 
 export default function Testimonials({sectionContentData}) {
 
@@ -19,15 +20,41 @@ export default function Testimonials({sectionContentData}) {
         }
     }
   ]
-  
+
+  const {arrowButton, titleContainer, arrowWrapper} = styles;
+
+  const [next, setNext] = useState(false);
+  const [back, setBack] = useState(false);
+
   return (
     <div>
-        <InfoBlock
-            titleText={title}
-            subtitleText={subtitle}
-            fromComponent="testimonials"
-        />
+        <div className={titleContainer}>
+          <InfoBlock
+              titleText={title}
+              subtitleText={subtitle}
+              fromComponent="testimonials"
+          />
+          <div className={arrowWrapper}>
+            <img
+                className={arrowButton}
+                onClick={()=>setBack(true)}
+                src="img/arrowBackButton.png"
+                alt="back button"
+              />
+              <img 
+                className={arrowButton} 
+                onClick={()=>setNext(true)} 
+                src="img/arrowNextButton.png" 
+                alt="next button" 
+                role="button" 
+              />
+          </div>
+        </div>
         <Carrousel
+            next={next}
+            setNext={setNext}
+            back={back}
+            setBack={setBack}
             images={testimonialsCards}
             imageContainer={styles.testimonialCardImage}
             DefaultSlidesToShow={2}
