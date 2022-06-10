ff038bd5fa0f298539d9d1ef5695745777601323
Carlos Giner <carlosginer.webdev@gmail.com>
2022-06-10 12:04:54 +0200

    Feature | Add footer background aand markup

--- layouts/Footer/index.jsx
+++ layouts/Footer/index.jsx
@@ -1,6 +1,16 @@
+import styles from './footer.module.scss';
 
 export default function Footer() {
+
+  const { container, contentBox, decoImage, footerEnd } = styles;
+  
   return (
-    <div></div>
+    <div className={container}>
+      <div className={contentBox}>
+      </div>
+      <div className={footerEnd}>
+      </div>
+      <img className={decoImage} src='img/deco-polygons.png' alt=''/>
+    </div>
   )
 }
