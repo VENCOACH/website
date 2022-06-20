7d9318e4ce0dda8e89ee563668db54f6fbe5c13b
Carlos Giner <carlosginer.webdev@gmail.com>
2022-06-20 20:55:25 +0200

    Feature | Finish footer end section

--- layouts/Footer/index.jsx
+++ layouts/Footer/index.jsx
@@ -1,10 +1,11 @@
 import styles from './footer.module.scss';
 import NavMenu from '../../components/navMenu';
 import menuData from "../../data/menus.json"
+import {Link} from 'react-scroll';
 
 export default function Footer() {
 
-  const { bannerFooter ,container, contentBox, decoImage, footerEnd, wrapper} = styles;
+  const { bannerFooter ,container, contentBox, decoImage, footerEnd, wrapper,logo, text, contentWrapper} = styles;
   
   return (
     <footer className={container}>
@@ -20,6 +21,21 @@ export default function Footer() {
         </div>
       </nav>
       <div className={footerEnd}>
+        <div className={contentWrapper}>
+          <figure>
+            <img className={logo} src="img/logo.png" alt="Vencoach Logo" />
+          </figure>
+          <p className={text}>Copyright © 2022, <span>Vencoach</span>. All rights Reserved. </p>
+          <Link
+            to={'navigationTop'}
+            offset={-200}
+            duration={1000}
+            spy={true}
+            smooth={true}
+          >
+            <img src={'img/subir.png'} alt='' />            
+          </Link>
+        </div>
       </div>
     </footer>
   )
